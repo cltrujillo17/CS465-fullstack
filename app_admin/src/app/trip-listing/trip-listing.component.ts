d01833d2d2371cdfc3eb56e58570e1a00ecc6d36
carlos trujillo <cltrujillo17@yahoo.com>
2026-02-23 02:03:18 -0500

    Module 6 Completed Baseline

---
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule} from '@angular/common';
+//import { trips } from '../data/trips';
+import { TripCardComponent } from '../trip-card/trip-card.component';
+
+import { Trip } from '../models/trip';
+import { TripDataService } from '../services/trip-data.service';
+
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-trip-listing',
+  standalone: true,
+  imports: [CommonModule, TripCardComponent],
+  templateUrl: './trip-listing.component.html',
+  styleUrl: './trip-listing.component.css',
+  providers: [TripDataService]
+})
+export class TripListingComponent implements OnInit {
+  //trips: Array<any> = trips;
+  trips: Trip[] = [];
+  message: string = '';
+
+  constructor(private tripDataService: TripDataService, private router: Router) {
+    console.log('TripListingComponent constructor');
+  }
+   
+  public addTrip(): void {
+    this.router.navigate(['/add-trip']);
+  }
+  private getStuff(): void {
+    this.tripDataService.getTrips()
+      .subscribe({
+        next: (value: any) => {
+          this.trips = value;
+          if(value.length > 0)
+          {
+            this.message = 'There are ' + value.length + ' tripsavailable.';
+          }
+          else{
+            this.message = 'There were no trips retireved from thedatabase';
+          }
+          console.log(this.message);
+        },
+        error: (error: any) => {
+          console.log('Error: ' + error);
+        }
+      })
+  }
+
+  ngOnInit(): void {
+    console.log('OnInit called');
+    this.getStuff();
+  }
+
+}
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -8,6 +8,12 @@ const tripsController = require('../controllers/trips'); // Import the trips con
 //define route for our trips endpoint, which will use the tripsController to handle GET requests to list all trips
 router
     .route('/trips')
-    .get(tripsController.tripsList); // GET /trips - lists all trips
+    .get(tripsController.tripsList)         // GET /trips - lists all trips
+    .post (tripsController.tripsAddTrip)    // POST /trips - creates a new trip
+    .put(tripsController.tripsUpdateTrip);  // PUT /trips - updates an existing trip
+
+router 
+    .route('/trips/:tripCode')
+    .get(tripsController.tripsFindByCode); // GET /trips/:tripCode - lists a single trip by its code
 
 module.exports = router;
